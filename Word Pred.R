8aab97e83b70cd43f9a8c4c0d61e1daacb355f82
leonardozg <zepeda.leonardo@gmail.com>
2020-05-24 16:35:29 -0500

    Add files via upload

--- Word Pred.R
+++ Word Pred.R
@@ -1,4 +1,22 @@
 
+#Load the n-gram databases
+
+unigramlist <- read.csv("unigram.csv")
+unigramlist <- as.data.table(unigramlist)
+unigramlist <- unigramlist[,-1]
+
+bigramlist <- read.csv("bigram.csv")
+bigramlist <- as.data.table(bigramlist)
+bigramlist <- bigramlist[,-1]
+
+trigramlist <- read.csv("trigram.csv")
+trigramlist <- as.data.table(trigramlist)
+trigramlist <- trigramlist[,-1]
+
+quadgramlist <- read.csv("quadgram.csv")
+quadgramlist <- as.data.table(quadgramlist)
+quadgramlist <- quadgramlist[,-1]
+
 
 #load the libraries
 library(ggplot2)
@@ -12,8 +30,9 @@ library(dplyr)
 library(tm)
 
 # 
-proc1 <-function(sentence){a <- inugramlist$unigram
-return(as.String(a[10]))
+proc1 <-function(sentence){
+  a <- inugramlist$unigram
+  return(as.String(a[10]))
 }
 
 proc <- function(sentence){
@@ -38,6 +57,51 @@ proc <- function(sentence){
   return(value)
 }
 
+quadgram <- function(fourg){
+  four <- paste(fourg,collapse = ' ')
+  foursum <- data.frame(fourgram="test",frequency=0)
+  k <- trigramlist[trigram==four]
+  m <- as.numeric(k$frequency)
+  if(length(m)==0) return(NULL)
+  
+  for(string0 in unigramlist$unigram){
+    text = paste(four,string0)
+    found <- quadgramlist[quadgram==text]
+    n<- as.numeric(found$frequency)
+    
+    if(length(n)!=0){
+      foursum <- rbind(foursum,found)
+      
+    }
+  }
+  if(nrow(foursum)==1) return(NULL)
+  foursum <- foursum[order(-frequency)]
+  sen <- unlist(strsplit(as.String(foursum[1,quadgram]),' '))
+  return (sen[length(sen)])
+}
+
+trigram <- function(threeg){
+  three <- paste(threeg,collapse = ' ')
+  threesum <- data.frame(trigram="test",frequency=0)
+  k <- bigramlist[bigram==three]
+  m <- as.numeric(k$frequency)
+  if(length(m)==0) return(NULL)
+  
+  for(string0 in unigramlist$unigram){
+    text = paste(three,string0)
+    found <- trigramlist[trigram==text]
+    n<- as.numeric(found$frequency)
+    
+    if(length(n)!=0){
+      threesum <- rbind(threesum,found)
+      
+    }
+  }
+  if(nrow(threesum)==1) return(NULL)
+  threesum <- threesum[order(-frequency)]
+  sen <- unlist(strsplit(as.String(threesum[1,trigram]),' '))
+  return (sen[length(sen)])
+}
 
 bigram <- function(twog){
   two <- paste(twog,collapse = ' ')
@@ -65,48 +129,3 @@ bigram <- function(twog){
   return (sen[length(sen)])
 }
 
-trigram <- function(threeg){
-  three <- paste(threeg,collapse = ' ')
-  threesum <- data.frame(trigram="test",frequency=0)
-  k <- bigramlist[bigram==three]
-  m <- as.numeric(k$frequency)
-  if(length(m)==0) return(NULL)
-  
-  for(string0 in unigramlist$unigram){
-    text = paste(three,string0)
-    found <- trigramlist[trigram==text]
-    n<- as.numeric(found$frequency)
-    
-    if(length(n)!=0){
-      threesum <- rbind(threesum,found)
-      
-    }
-  }
-  if(nrow(threesum)==1) return(NULL)
-  threesum <- threesum[order(-frequency)]
-  sen <- unlist(strsplit(as.String(threesum[1,trigram]),' '))
-  return (sen[length(sen)])
-}
-
-quadgram <- function(fourg){
-  four <- paste(fourg,collapse = ' ')
-  foursum <- data.frame(fourgram="test",frequency=0)
-  k <- trigramlist[trigram==four]
-  m <- as.numeric(k$frequency)
-  if(length(m)==0) return(NULL)
-  
-  for(string0 in unigramlist$unigram){
-    text = paste(four,string0)
-    found <- quadgramlist[quadgram==text]
-    n<- as.numeric(found$frequency)
-    
-    if(length(n)!=0){
-      foursum <- rbind(foursum,found)
-      
-    }
-  }
-  if(nrow(foursum)==1) return(NULL)
-  foursum <- foursum[order(-frequency)]
-  sen <- unlist(strsplit(as.String(foursum[1,quadgram]),' '))
-  return (sen[length(sen)])
-}
